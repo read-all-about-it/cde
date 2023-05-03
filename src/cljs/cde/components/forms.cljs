9a6aec47329dce5832d2724991453b49a1cd47dc
Galen Cuthbertson <g@galen.me>
2023-05-03 15:20:29 +1000

    Tinker with search input fields

--- src/cljs/cde/components/forms.cljs
+++ src/cljs/cde/components/forms.cljs
@@ -29,30 +29,45 @@
              :on-change #(rf/dispatch [:search/update-query :newspaper-title (-> % .-target .-value)])}]]]]]
        [:div.field.is-horizontal
         [:div.field-body
-         [:div.field.
+         [:div.field
           [:div.control
            [:input.input
             {:type "text"
-             :placeholder "Search by author..."
+             :placeholder "Search by author name..."
              :value (:author @query)
              :on-change #(rf/dispatch [:search/update-query :author (-> % .-target .-value)])}]]]
          [:div.field
-          [:div.control
+          [:div.control.has-icons-left
            [:div.select
             [:select
              {:value (:nationality @query)
               :on-change #(rf/dispatch [:search/update-query :nationality (-> % .-target .-value)])}
-             [:option {:value "" :disabled true :selected true} "Select nationality"]
+             [:option {:value "" :disabled true :selected true} "Author Nationality"]
              ; Add more options for each nationality
              [:option {:value "Australian"} "Australian"]
-             [:option {:value "British"} "British"]]]]]
+             [:option {:value "British"} "British"]]
+            [:span.icon.is-small.is-left
+             [:i.material-icons "public"]]]]]
          [:div.field
           [:div.control
            [:div.select
             [:select
              {:value (:gender @query)
               :on-change #(rf/dispatch [:search/update-query :gender (-> % .-target .-value)])}
-             [:option {:value "" :disabled true :selected true} "Select gender"]
+             [:option {:value "" :disabled true :selected true} "Author Gender"]
              ; Add more options for each gender
              [:option {:value "Male"} "Male"]
-             [:option {:value "Female"} "Female"]]]]]]]])))
+             [:option {:value "Female"} "Female"]]]]]
+         [:div.field
+          [:div.control
+           [:div.select 
+            [:select
+             {:value (:length @query)
+              :on-change #(rf/dispatch [:search/update-query :length (-> % .-target .-value)])} 
+             [:option {:value "" :disabled true :selected true} "Story Length"]
+             ;; TODO: Fix these options
+             [:option {:value ""} "Any"]
+             [:option {:value 0} "Serialised Title"]
+             [:option {:value 1} "Short Single Edition Story"]
+             [:option {:value 8} "Long Single Edition Story"]]]]]
+         ]]])))
