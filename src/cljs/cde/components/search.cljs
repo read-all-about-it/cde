f7006713cf660bd68c334492b09a5ebf962f10f2
Galen Cuthbertson <g@galen.me>
2023-05-11 13:47:56 +1000

    Tinker with search form field structure

--- src/cljs/cde/components/search.cljs
+++ src/cljs/cde/components/search.cljs
@@ -13,6 +13,7 @@
                error (r/atom nil)]
     (fn []
       [:div
+      [:div
        [:div.field
         {:style {:text-align "center"}}
         [:input.switch.is-rtl.is-rounded
@@ -25,9 +26,9 @@
         [:label
          {:for "chapter-switch"}
          "Search Chapters?"]]
-       (if-not @search-chapters?
-         [:div
-          [:div.field.is-horizontal
+       [:div
+        [:div.field.is-horizontal
+         (if-not @search-chapters?
            [:div.field-body
             [:div.field
              [:div.control
@@ -42,44 +43,53 @@
                {:type "text"
                 :placeholder "Search by newspaper title..."
                 :value (:newspaper-title @query)
-                :on-change #(rf/dispatch [:search/update-query :newspaper-title (-> % .-target .-value)])}]]]]]
-          [:div.field.is-horizontal
+                :on-change #(rf/dispatch [:search/update-query :newspaper-title (-> % .-target .-value)])}]]]]
            [:div.field-body
             [:div.field
              [:div.control
               [:input.input
                {:type "text"
-                :placeholder "Search by author name..."
-                :value (:author @query)
-                :on-change #(rf/dispatch [:search/update-query :author (-> % .-target .-value)])}]]]
-            [:div.field
-             [:div.control.has-icons-left
-              [:div.select
-               [:select
-                {:value (:nationality @query)
-                 :on-change #(rf/dispatch [:search/update-query :nationality (-> % .-target .-value)])}
-                [:option {:value "" :disabled true :selected true} "Author Nationality"]
+                :placeholder "Search for chapter text..."
+                :value (:common-title @query)
+                :on-change #(rf/dispatch [:search/update-query :chapter-text (-> % .-target .-value)])}]]]])]
+        [:div.field.is-horizontal
+         [:div.field-body
+          [:div.field
+           [:div.control
+            [:input.input
+             {:type "text"
+              :placeholder "Search by author name..."
+              :value (:author @query)
+              :on-change #(rf/dispatch [:search/update-query :author (-> % .-target .-value)])}]]]
+          [:div.field
+           [:div.control.has-icons-left
+            [:div.select
+             [:select
+              {:value (:nationality @query)
+               :on-change #(rf/dispatch [:search/update-query :nationality (-> % .-target .-value)])}
+              [:option {:value "" :disabled true :selected true} "Author Nationality"]
              ; Add more options for each nationality
-                [:option {:value ""} "Any"]
-                [:option {:value "Australian"} "Australian"]
-                [:option {:value "British"} "British"]]
-               [:span.icon.is-small.is-left
-                [:i.material-icons "public"]]]]]
-            [:div.field
-             [:div.control.has-icons-left
-              [:div.select
-               [:select
-                {:value (:gender @query)
-                 :on-change #(rf/dispatch [:search/update-query :gender (-> % .-target .-value)])}
-                [:option {:value "" :disabled true :selected true} "Author Gender"]
+              [:option {:value ""} "Any"]
+              [:option {:value "Australian"} "Australian"]
+              [:option {:value "British"} "British"]]
+             [:span.icon.is-small.is-left
+              [:i.material-icons "public"]]]]]
+          [:div.field
+           [:div.control.has-icons-left
+            [:div.select
+             [:select
+              {:value (:gender @query)
+               :on-change #(rf/dispatch [:search/update-query :gender (-> % .-target .-value)])}
+              [:option {:value "" :disabled true :selected true} "Author Gender"]
              ; Add more options for each gender
-                [:option {:value ""} "Any"]
-                [:option {:value "Female"} "Female"]
-                [:option {:value "Male"} "Male"]
-                [:option {:value "Multiple"} "Multiple"]
-                [:option {:value "Unknown"} "Unknown"]]
-               [:span.icon.is-small.is-left
-                [:i.material-icons "transgender"]]]]]
+              [:option {:value ""} "Any"]
+              [:option {:value "Female"} "Female"]
+              [:option {:value "Male"} "Male"]
+              [:option {:value "Multiple"} "Multiple"]
+              [:option {:value "Unknown"} "Unknown"]]
+             [:span.icon.is-small.is-left
+              [:i.material-icons "transgender"]]]]]
+          (when-not @search-chapters?
             [:div.field
              [:div.control.has-icons-left
               [:div.select
@@ -93,18 +103,7 @@
                 [:option {:value 1} "Short Single Edition"]
                 [:option {:value 8} "10,000+ Words (Single Edition)"]]
                [:span.icon.is-small.is-left
-                [:i.material-icons "auto_stories"]]]]]]]]
-         [:div
-          [:div.field.is-horizontal
-           [:div.field-body
-            [:div.field
-             [:div.control
-              [:input.input
-               {:type "text"
-                :placeholder "Search for chapter text..."
-                :value (:common-title @query)
-                :on-change #(rf/dispatch [:search/update-query :chapter-text (-> % .-target .-value)])}]]]
-            ]]])
+                [:i.material-icons "auto_stories"]]]]])]]]]
        ;; the 'search' button, which (on click) will dispatch the submit-search
        ;; event (allowing the 'search-result' component placed below this on
        ;; on the search page to start populating results as they come in from
